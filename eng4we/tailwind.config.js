5af58a914a0fe53883e4526c8fd15ec845fd6690
Kuzey <keyildiz29@my.aci.k12.tr>
2025-07-23 16:45:54 +0300

    fix tailwindcss

--- eng4we/tailwind.config.js
+++ eng4we/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./index.html", // or wherever your root HTML is
+    "./src/**/*.{js,jsx,ts,tsx}", // all your React components
+  ],
   theme: {
     extend: {},
   },
